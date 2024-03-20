970bfebe28104d713323ec8a6e81170bb5465377
DreamSnow607 <227729396@qq.com>
2024-03-20 21:02:33 +0800

    按钮启动一下

--- reminder_app.py
+++ reminder_app.py
@@ -4,15 +4,24 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.core.text import LabelBase
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+
+from reminder_cmd import * # 作为库文件使用
 
 LabelBase.register(name='Default',
                    fn_regular='STXIHEI.ttf')
 
 kivy.require("2.3.0")
 
+class MyLayout(BoxLayout):
+    def print_result(self):
+        inv=get_inventory(datetime.date.today())
+        self.ids.main_label.text=inv.to_kivy_text()
 
 class ReminderWindowsApp(App):
     pass
+
 
 if __name__ == "__main__":
     app=ReminderWindowsApp()
